0912ff24f5ef46787a66e2c5de51b75381b1c8e3
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-05-18 07:44:35 -0500

    fotos index-4

--- src/pages/index-4.js
+++ src/pages/index-4.js
@@ -43,7 +43,7 @@ const Home4 = () => {
       <LiveSectionTwo />
       <SponsorsSectionThree />
       <TestimonialsSectionThree />
-      <NewsSectionTwo />
+      {/* <NewsSectionTwo /> */}
       <CallToSectionThree />
       <FooterSeven />
     </Layout>
